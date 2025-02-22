8515589f0fc2f8bbe58d30f65a90738b7f62622c
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2025-02-22 19:56:18 +0000

    Added HashObject and CatFile functions

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -19,7 +21,31 @@ func main() {
 		},
 	}
 
+	var hashObjectCommand = &cobra.Command{
+		Use:   "hash-object",
+		Short: "Hash the file contents of the given file",
+		Run: func(_ *cobra.Command, args []string) {
+			if len(args) > 1 {
+				log.Fatal(fmt.Errorf("Invalid number of arguments"))
+			}
+			hashObject(args[0])
+		},
+	}
+
+	var catFileCommand = &cobra.Command{
+		Use:   "cat-file",
+		Short: "Prints the content of an object",
+		Run: func(_ *cobra.Command, args []string) {
+			if len(args) > 1 {
+				log.Fatal(fmt.Errorf("Invalid number of arguments"))
+			}
+			catFile(args[0])
+		},
+	}
+
 	rootCmd.AddCommand(initCommand)
+	rootCmd.AddCommand(hashObjectCommand)
+	rootCmd.AddCommand(catFileCommand)
 	rootCmd.Execute()
 }
 
@@ -28,6 +54,22 @@ func goInit() {
 	if err != nil {
 		panic(-1)
 	}
-	fmt.Printf("Initialized empty gogit repository in %s/%s", currDir, GitDir)
+	fmt.Printf("Initialized empty gogit repository in %s", filepath.Join(currDir, GitDir))
 	Init()
+}
+
+func hashObject(filepath string) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	HashObject(data)
+}
+
+func catFile(hashString string) {
+	data := GetObject(hashString)
+	_, err := os.Stdout.Write(data)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- data.go
+++ data.go
@@ -2,7 +2,12 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 // GitDir Git directory name
@@ -11,4 +16,25 @@ const GitDir string = ".gogit"
 // Init Initializes the gogit directory
 func Init() {
 	os.Mkdir(GitDir, 0755)
+	os.Mkdir(filepath.Join(GitDir, "objects"), 0755)
+}
+
+// HashObject hashes file contents to a unique blob
+func HashObject(data []byte) {
+	hash := sha256.New()
+	hash.Write(data)
+	byteHash := hash.Sum(nil)
+	stringHash := hex.EncodeToString(byteHash)
+
+	os.WriteFile(filepath.Join(GitDir, "objects", stringHash), data, 0444)
+	fmt.Println(stringHash)
+}
+
+// GetObject returns the contents of a file based on its hash
+func GetObject(hash string) (data []byte) {
+	data, err := os.ReadFile(filepath.Join(GitDir, "objects", hash))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
 }
